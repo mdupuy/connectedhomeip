382bad29386b43c85e6fe1b187807d7f8ab4393b
Matthew DuPuy <matthew.dupuy@arm.com>
2022-06-30 23:54:26 -0700

    avh-raspi.py

--- avh-raspi.py
+++ avh-raspi.py
@@ -18,13 +18,13 @@ if len(sys.argv) < 3:
   print('Usage: %s <ApiEndpoint> <ApiToken> [vmName]', sys.argv[0])
   exit(-1)
 
-apiEndpoint = os.environment['ENDPOINT'] #sys.argv[1]
-apiToken = os.environment['API_TOKEN'] #sys.argv[2]
+apiEndpoint = os.environment['ENDPOINT'] #sys.argv[1] #"https://app.avh.arm.com/api"     
+apiToken = os.environment['API_TOKEN'] #sys.argv[2] #51f6cc92c0c4c08072a0.93da8450ac6e901a6fa2c95b4f044926623bd92eda46c2cdb54caf97f79851c147a3a900e22e7be50363d2c47d857351fd0faec319b58411895a1fe1819392ce
 #if len(sys.argv) > 3:
 #  vmName = sys.argv[3]
 #else:
-vmName = os.environment['INSTANCE1']
-vmName2 = os.environment['INSTANCE2']
+vmName = os.environment['INSTANCE1']  #chip-tool
+vmName2 = os.environment['INSTANCE2'] #light
 
 async def waitForState(instance, state):
   global api_instance
@@ -140,13 +140,13 @@ async def main():
     api_response = await api_instance.v1_get_model_software(model.model)
     chosenSoftware = None
     for software in api_response:
-      pprint(software)
-      #if software.filename.startswith('STM32U5-WiFiBasics'):
-      #  # This software package is compatible with our bsp.elf image
-      #  chosenSoftware = software
-      #  break
+      #pprint(software)
+      if software.filename.startswith('rpi4b-11.2-lite.coreimg-88715747-93d8-4d48-b8af-94507662df9d'):
+        # This software package is compatible with our bsp.elf image
+        chosenSoftware = software
+        break
 
-    print('Creating a new instance...')
+    print('Creating a chip-test instance...')
     api_response = await api_instance.v1_create_instance({
       "name": vmName,
       "project": projectId,
@@ -156,10 +156,21 @@ async def main():
     })
     instance = api_response
 
+    print('Creating a instance...')
+    api_response = await api_instance.v1_create_instance({
+      "name": vmName2,
+      "project": projectId,
+      "flavor": chosenModel.flavor,
+      "os": chosenSoftware.version,
+      "osbuild": chosenSoftware.buildid
+    })
+    instance2 = api_response
+
     error = None
     try:
-      print('Waiting for VM to create...')
+      print('Waiting for VMs to create...')
       await waitForState(instance, 'on')
+      await waitForState(instance2, 'on')
 
       print('Setting the VM to use the bsp test software')
       #api_response = await api_instance.v1_create_image('fwbinary', 'plain', 
@@ -169,19 +180,19 @@ async def main():
       #)
       #pprint(api_response)
 
-      print('Resetting VM to use the new software')
-      api_response = await api_instance.v1_reboot_instance(instance.id)
-      print('Waiting for VM to finish resetting...')
-      await waitForState(instance, 'on')
-      print('done')
-      print('Logging GPIO initial state:')
-      gpios = await api_instance.v1_get_instance_gpios(instance.id)
-      pprint(gpios)
-      print('running test')
-      await testBspImage(instance)
-      print('Logging GPIO final state:')
-      gpios = await api_instance.v1_get_instance_gpios(instance.id)
-      pprint(gpios)
+      #print('Resetting VM to use the new software')
+      #api_response = await api_instance.v1_reboot_instance(instance.id)
+      #print('Waiting for VM to finish resetting...')
+      #await waitForState(instance, 'on')
+      #print('done')
+      #print('Logging GPIO initial state:')
+      #gpios = await api_instance.v1_get_instance_gpios(instance.id)
+      #pprint(gpios)
+      #print('running test')
+      #await testBspImage(instance)
+      #print('Logging GPIO final state:')
+      #gpios = await api_instance.v1_get_instance_gpios(instance.id)
+      #pprint(gpios)
 
     except Exception as e:      
       print('Encountered error; cleaning up...')
@@ -189,6 +200,7 @@ async def main():
 
     print('Deleting instance...')
     api_response = await api_instance.v1_delete_instance(instance.id)
+    api_response = await api_instance.v1_delete_instance(instance2.id)
 
     if error != None:
       raise error
